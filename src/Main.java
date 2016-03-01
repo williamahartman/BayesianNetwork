b4b3963ac9e95b8a6781edaba48764fbd0e6e950
Will Hartman <william.a.hartman@gmail.com>
2016-02-29 22:10:41 -0500

    Added the framework for running it

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,33 @@
-import java.util.Arrays;
 import java.util.Map;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Map<String, Node> net = FileReader.readNetwork("network_option_b.txt");
-        FileReader.readVariables("query1.txt", net);
+    public static double rejectionSampling(Map<String, Node> network, int numSamples) {
+        return 0;
+    }
 
-        System.out.println(net.get("node1").getProbabilityGivenParents(Arrays.asList(net.get("node4"))));
-        System.out.println(net.get("node6").getVariableInfo());
+    public static double likelihoodWeightingSampling(Map<String, Node> network, int numSamples) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int numSamples = -1;
+        if(args.length == 3) {
+            try {
+                numSamples = Integer.parseInt(args[2]);
+            } catch (NumberFormatException e) {
+                System.err.println("The input was not an integer");
+                System.exit(-1);
+            }
+        } else {
+            System.err.println("This program must be run with the number of samples as an argument");
+            System.exit(-1);
+        }
+
+        Map<String, Node> net = FileReader.readNetwork(args[0]);
+        FileReader.readVariables(args[1], net);
+
+        System.out.println("Rejection Sampling for " + numSamples + " samples: " + rejectionSampling(net, numSamples));
+        System.out.println("Likelihood Weighting Sampling for " + numSamples + " samples: " + likelihoodWeightingSampling(net, numSamples));
     }
 }
